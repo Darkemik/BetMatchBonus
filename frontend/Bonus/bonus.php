7193815014ad8168693732ab342548bacd84b36f
Neon <urban.marcell44@gmail.com>
2026-02-11 12:14:03 +0100

    a gyik oldal css-ét továbbfejlesztettem

--- frontend/Bonus/bonus.php
+++ frontend/Bonus/bonus.php
@@ -20,25 +20,20 @@
 
 
         <nav class="nav">
-    <a href="../../frontend/MainMenu/MainMenu.php" id="fooldalszoveg" 
-       onmouseover="this.style.color='#ffc89b'" 
-       onmouseout="this.style.color=''">Főoldal</a>
-       
-    <a href="../../frontend/Live/live.php" id="eloszoveg"
-       onmouseover="this.style.color='#ffc89b'"
-       onmouseout="this.style.color=''">Élő</a>
-       
-    <a href="../../frontend/Bonus/bonus.php" id="bonuszszoveg" 
-       style="color: orange;">Bónuszok</a>
-       
-    <a href="../../frontend/Help/help.php" id="segitsegszoveg"
-       onmouseover="this.style.color='#ffc89b'"
-       onmouseout="this.style.color=''">Segítség</a>
-</nav>
+            <a href="../../frontend/MainMenu/MainMenu.php" id="fooldalszoveg" onmouseover="this.style.color='#ffc89b'"
+                onmouseout="this.style.color=''">Főoldal</a>
+
+            <a href="../../frontend/Live/live.php" id="eloszoveg" onmouseover="this.style.color='#ffc89b'"
+                onmouseout="this.style.color=''">Élő</a>
+
+            <a href="../../frontend/Bonus/bonus.php" id="bonuszszoveg" style="color: orange;">Bónuszok</a>
+
+            <a href="../../frontend/Help/help.php" id="segitsegszoveg" onmouseover="this.style.color='#ffc89b'"
+                onmouseout="this.style.color=''">Segítség</a>
+        </nav>
         <div class="right_side">
             <button class="loginbtn" onclick="location.href='../../frontend/Login/login.php'">Bejelentkezés</button>
-            <button class="registrationbtn"
-                onclick="location.href='../../frontend/Register/register.php'">Regisztráció</button>
+            <button class="registrationbtn" onclick="location.href='../../frontend/Register/register.php'">Regisztráció</button>
         </div>
     </header>
     <div class="container">
@@ -53,8 +48,8 @@
                         <strong>Töltsön fel minimum 5000 FT ÉRTÉKBEN ÉS DUPLÁZZA MEG A BEFIZETT ÖSSZEGÉT</strong>
                     </div>
                     <p class="doboz-szoveg"></p>
-                    <button class="tobb-info-gomb" id="tobbInfoGomb">Több információ</button>
                     <a href="#" class="doboz-gomb">BEJELENTKEZÉS / REGISZTRÁCIÓ</a>
+                    <button class="tobb-info-gomb" id="tobbInfoGomb">Több információ</button>
                 </div>
             </div>
 
@@ -66,9 +61,10 @@
                     <div class="bonus-feltetel">
                         <strong>Fogadjon legalább 5-ös kötésben és kapjon extra szorzót a kötésére</strong>
                     </div>
-                    <p class="doboz-szoveg">A szelvény piacai szorzói minimum legyen fejenként 1.3 (Így ha például 5 ös kötésben 5*1,3 akkor mi ehhez hozzá teszünk még 1,3-as oddsot a végső odds: 7,8!)</p>
-                    <button class="tobb-info-gomb" id="tobbInfoGomb">Több információ</button>
-                    <a href="#" class="doboz-gomb">BEJELENTKEZÉS / REGISZTRÁCIÓ</a>
+                    <p class="doboz-szoveg">A szelvény piacai szorzói minimum legyen fejenként 1.3 (Így ha például 5 ös
+                        kötésben 5*1,3 akkor mi ehhez hozzá teszünk még 1,3-as oddsot a végső odds: 7,8!)</p>
+                        <a href="#" class="doboz-gomb">BEJELENTKEZÉS / REGISZTRÁCIÓ</a>
+                        <button class="tobb-info-gomb" id="tobbInfoGomb">Több információ</button>
                 </div>
             </div>
 
@@ -82,8 +78,8 @@
                         <strong>Fogadjon 2-es kötésben és a szelvénye eredő oddsa legyen 2.</strong>
                     </div>
                     <p class="doboz-szoveg">Kapjon 10.000FT ingyenes fogadást melyet bármire tehet.</p>
-                    <button class="tobb-info-gomb" id="tobbInfoGomb">Több információ</button>
                     <a href="#" class="doboz-gomb">BEJELENTKEZÉS / REGISZTRÁCIÓ</a>
+                    <button class="tobb-info-gomb" id="tobbInfoGomb">Több információ</button>
                 </div>
             </div>
 
@@ -97,8 +93,8 @@
                         <strong>Speciális fogadások amelyeket mi teszünk ki, minden nap fixen 1-et!</strong>
                     </div>
                     <p class="doboz-szoveg"></p>
-                    <button class="tobb-info-gomb" id="tobbInfoGomb">Több információ</button>
                     <a href="#" class="doboz-gomb">BEJELENTKEZÉS / REGISZTRÁCIÓ</a>
+                    <button class="tobb-info-gomb" id="tobbInfoGomb">Több információ</button>
                 </div>
             </div>
         </div>
@@ -113,8 +109,8 @@
                         <strong>A nap legfontosabb foci mérkőzésére az 1X2 piacra megnöveljük a szorzót!</strong>
                     </div>
                     <p class="doboz-szoveg"></p>
-                    <button class="tobb-info-gomb" id="tobbInfoGomb">Több információ</button>
                     <a href="#" class="doboz-gomb">BEJELENTKEZÉS / REGISZTRÁCIÓ</a>
+                    <button class="tobb-info-gomb" id="tobbInfoGomb">Több információ</button>
                 </div>
             </div>
 
@@ -128,8 +124,8 @@
                         <strong>2-es szorzó, 2-es kötés</strong>
                     </div>
                     <p class="doboz-szoveg">Töltsön fel 5000FT-ot és mi azt megduplázzuk!</p>
-                    <button class="tobb-info-gomb" id="tobbInfoGomb">Több információ</button>
                     <a href="#" class="doboz-gomb">BEJELENTKEZÉS / REGISZTRÁCIÓ</a>
+                    <button class="tobb-info-gomb" id="tobbInfoGomb">Több információ</button>
                 </div>
             </div>
 
@@ -144,8 +140,8 @@
                     </div>
                     <p class="doboz-szoveg">NBA és EuroLeague mérkőzéseken minden fogadáshoz +0.5 odds bónuszt kap
                         automatikusan!</p>
-                    <button class="tobb-info-gomb" id="tobbInfoGomb">Több információ</button>
-                    <a href="#" class="doboz-gomb">BEJELENTKEZÉS / REGISZTRÁCIÓ</a>
+                        <a href="#" class="doboz-gomb">BEJELENTKEZÉS / REGISZTRÁCIÓ</a>
+                        <button class="tobb-info-gomb" id="tobbInfoGomb">Több információ</button>
                 </div>
             </div>
 
@@ -154,10 +150,12 @@
                 <img src="../../img/cashout.jpeg" alt="Cash out" class="doboz-kep">
                 <div class="doboz-tartalom">
                     <p class="doboz-cim">CASH OUT - AZONNALI KIFIZETÉS</p>
-                        <strong>Fogadásának mérkőzésektől függően pénzt vehet ki!</strong>
-                        <p class="doboz-szoveg">Amennyiben a meccs/meccsek még nem indultak el 80%ban kiveheti azt. Ha a szelvénye bukásfelé áll ez már csak 20%, viszont ha a szelvény nyerésre áll azt kiveheti 130%os profittal.</p>
-                    <button class="tobb-info-gomb" id="tobbInfoGomb">Több információ</button>
-                    <a href="#" class="doboz-gomb">BEJELENTKEZÉS / REGISZTRÁCIÓ</a>
+                    <strong>Fogadásának mérkőzésektől függően pénzt vehet ki!</strong>
+                    <p class="doboz-szoveg">Amennyiben a meccs/meccsek még nem indultak el 80%ban kiveheti azt. Ha a
+                        szelvénye bukásfelé áll ez már csak 20%, viszont ha a szelvény nyerésre áll azt kiveheti 130%os
+                        profittal.</p>
+                        <a href="#" class="doboz-gomb">BEJELENTKEZÉS / REGISZTRÁCIÓ</a>
+                        <button class="tobb-info-gomb" id="tobbInfoGomb">Több információ</button>
                 </div>
             </div>
         </div>
@@ -166,7 +164,7 @@
     <footer class="simple-footer">
         <div class="footer-top">
             <div class="footer-links">
-                <a href="../Help/adatkezelesi_tajékoztatok.php" class="footer-link">ADATKEZELÉSI TÁJÉKOZTATÓ</a>
+                <a href="../Help/adatkezelesi_tajekoztatok.php" class="footer-link">ADATKEZELÉSI TÁJÉKOZTATÓ</a>
                 <a href="../Help/reszveteli-szabalyzat.php" class="footer-link">RÉSZVÉTELI SZABÁLYZAT</a>
                 <a href="../Help/kapcsolat.php" class="footer-link">UGYFELSZOLGALAT@BETMATCHBONUS.COM</a>
                 <a href="../Help/GYIK.php" class="footer-link">GYIK</a>
@@ -186,6 +184,7 @@
             </div>
         </div>
     </footer>
+
 </body>
 
 </html>
