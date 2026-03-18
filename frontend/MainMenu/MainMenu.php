4f3096f86a5ba51893b1d0217eed3a37dcc8ecd8
Neon <urban.marcell44@gmail.com>
2026-03-18 11:42:48 +0100

    új dizájn, bejelentkezett fiók jobb oldalon van az iconja szebb layout és átláthatóbb.

--- frontend/MainMenu/MainMenu.php
+++ frontend/MainMenu/MainMenu.php
@@ -84,23 +84,8 @@ if (!$matchesResult) {
                         </button>
                     </div>
                 </div>
-            <button class="loginbtn" data-bs-toggle="modal" data-bs-target="#loginModal">Bejelentkezés</button>
-            <button class="registrationbtn" data-bs-toggle="modal" data-bs-target="#registerModal">Regisztráció</button>
+            <?php include '../../frontend/Components/login.php'; ?>
         </div>
-        <div id="userMenu" class="dropdown" style="display:none;">
-  <button class="btn btn-outline-light dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-    <span id="userMenuUsername">Fiókom</span>
-  </button>
-
-  <ul class="dropdown-menu dropdown-menu-end">
-    <li class="dropdown-header">
-      <div><strong id="userFullName">-</strong></div>
-      <div style="font-size: 12px;" id="userEmail">-</div>
-    </li>
-    <li><hr class="dropdown-divider"></li>
-    <li><button class="dropdown-item" id="logoutBtn" type="button">Kijelentkezés</button></li>
-  </ul>
-</div>
     </div>
 
     <nav class="nav collapse navbar-collapse" id="mainNavbar">
